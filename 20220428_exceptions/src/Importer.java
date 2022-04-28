ef58b43b6315e0aedab6aff2c3665a108fa8364e
laundy <laundy@users.noreply.github.com>
2022-04-28 18:29:09 +0200

    Beispiel für Behandlung von Exceptions

--- 20220428_exceptions/src/Importer.java
+++ 20220428_exceptions/src/Importer.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,23 +8,29 @@ public class Importer {
     public static Map<String, BankAccount> readBankAccounts(String filename) {
         Map<String, BankAccount> bankAccounts = new HashMap<>();
 
-        File importFile = new File(filename);
-        FileReader fileReader = new FileReader(importFile);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        try {
+            File importFile = new File(filename);
+            FileReader fileReader = new FileReader(importFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            String[] lineParts = line.split(":");
-            if (lineParts.length != 3) {
-                // FileFormatException
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] lineParts = line.split(":");
+                if (lineParts.length != 3) {
+                    // FileFormatException
+                }
+                BankAccount bankAccount = new BankAccount(lineParts[0], Double.parseDouble(lineParts[1]), Integer.parseInt(lineParts[2]) == 1);
+                bankAccounts.put(bankAccount.getIban(), bankAccount);
             }
-            BankAccount bankAccount = new BankAccount(lineParts[0], Double.parseDouble(lineParts[1]), Integer.parseInt(lineParts[2]) == 1);
-            bankAccounts.put(bankAccount.getIban(), bankAccount);
+
+            bufferedReader.close();
+
+            System.out.printf("Die Kontoinformationen für %d Kunden wurden erfolgreich geladen.\n", bankAccounts.size());
+        } catch (FileNotFoundException e) {
+            System.err.printf("Die Datei %s wurde nicht gefunden.\n", filename);
+        } catch (IOException e) {
+            System.err.println("Beim Lesen der Datei ist ein Fehler aufgetreten.");
         }
-
-        bufferedReader.close();
-
-        System.out.printf("Die Kontoinformationen für %d Kunden wurden erfolgreich geladen.\n", bankAccounts.size());
 
         return bankAccounts;
     }
--- 20220428_exceptions/src/JavaBankAG.java
+++ 20220428_exceptions/src/JavaBankAG.java
@@ -16,37 +16,41 @@ public class JavaBankAG {
         String userInput;
         while (true) {
             userInput = printMenuAndAskForAction();
-            switch (userInput) {
-                case "1":
-                    getBankAccountForIban("Geben Sie die IBAN des Kontos ein:").printDetails();
-                    break;
-                case "2":
-                    BankAccount depositBankAccount = getBankAccountForIban("Geben Sie die IBAN des Kontos ein, für das Sie die Einzahlung vornehmen möchten:");
-                    double amountToDeposit = askForDouble("Welcher Betrag soll eingezahlt werden (Format: _.__)?");
-                    depositBankAccount.deposit(amountToDeposit);
-                    System.out.printf("Der Betrag in Höhe von %.2f € wurde dem Konto mit der IBAN %s gutgeschrieben.\n", amountToDeposit, depositBankAccount.getIban());
-                    break;
-                case "3":
-                    BankAccount debitBankAccount = getBankAccountForIban("Geben Sie die IBAN des Kontos ein, für das Sie die Auszahlung vornehmen möchten:");
-                    double amountToDebit = askForDouble("Welcher Betrag soll abgebucht werden (Format: _.__)?");
-                    if (debitBankAccount.debit(amountToDebit)) {
-                        System.out.printf("Der Betrag in Höhe von %.2f € wurde vom Konto mit der IBAN %s abgebucht.\n", amountToDebit, debitBankAccount.getIban());
-                    } else {
-                        System.err.printf("Der Betrag in Höhe von %.2f € konnte nicht vom Konto mit der IBAN %s abgebucht werden, da die Deckung nicht ausreicht und keine Überziehung erlaubt ist.\n",
-                                amountToDebit, debitBankAccount.getIban());
-                    }
-                    break;
-                case "4":
-                    BankAccount selectedBankAccount = getBankAccountForIban("Geben Sie die IBAN des Kontos ein, für das Sie die Kontoüberziehungseinstellungen anpassen möchten.");
-                    System.out.printf("Das Konto mit der IBAN %s erlaubt%sÜberziehung.", selectedBankAccount.getIban(), selectedBankAccount.allowsOverdraft() ? "" : " keine ");
-                    String newOptionValue = askForString("Soll eine Überziehung für das Konto erlaubt werden (ja/nein)?");
-                    selectedBankAccount.setOverdraft(newOptionValue.equals("ja"));
-                    break;
-                case "e":
-                    printFarewell();
-                    System.exit(0);
-                default:
-                    System.err.println("Die Eingabe wurde nicht erkannt, bitte versuchen Sie es nocheinmal.");
+            try {
+                switch (userInput) {
+                    case "1":
+                        getBankAccountForIban("Geben Sie die IBAN des Kontos ein:").printDetails();
+                        break;
+                    case "2":
+                        BankAccount depositBankAccount = getBankAccountForIban("Geben Sie die IBAN des Kontos ein, für das Sie die Einzahlung vornehmen möchten:");
+                        double amountToDeposit = askForDouble("Welcher Betrag soll eingezahlt werden (Format: _.__)?");
+                        depositBankAccount.deposit(amountToDeposit);
+                        System.out.printf("Der Betrag in Höhe von %.2f € wurde dem Konto mit der IBAN %s gutgeschrieben.\n", amountToDeposit, depositBankAccount.getIban());
+                        break;
+                    case "3":
+                        BankAccount debitBankAccount = getBankAccountForIban("Geben Sie die IBAN des Kontos ein, für das Sie die Auszahlung vornehmen möchten:");
+                        double amountToDebit = askForDouble("Welcher Betrag soll abgebucht werden (Format: _.__)?");
+                        if (debitBankAccount.debit(amountToDebit)) {
+                            System.out.printf("Der Betrag in Höhe von %.2f € wurde vom Konto mit der IBAN %s abgebucht.\n", amountToDebit, debitBankAccount.getIban());
+                        } else {
+                            System.err.printf("Der Betrag in Höhe von %.2f € konnte nicht vom Konto mit der IBAN %s abgebucht werden, da die Deckung nicht ausreicht und keine Überziehung erlaubt ist.\n",
+                                    amountToDebit, debitBankAccount.getIban());
+                        }
+                        break;
+                    case "4":
+                        BankAccount selectedBankAccount = getBankAccountForIban("Geben Sie die IBAN des Kontos ein, für das Sie die Kontoüberziehungseinstellungen anpassen möchten.");
+                        System.out.printf("Das Konto mit der IBAN %s erlaubt%sÜberziehung.", selectedBankAccount.getIban(), selectedBankAccount.allowsOverdraft() ? "" : " keine ");
+                        String newOptionValue = askForString("Soll eine Überziehung für das Konto erlaubt werden (ja/nein)?");
+                        selectedBankAccount.setOverdraft(newOptionValue.equals("ja"));
+                        break;
+                    case "e":
+                        printFarewell();
+                        System.exit(0);
+                    default:
+                        System.err.println("Die Eingabe wurde nicht erkannt, bitte versuchen Sie es nocheinmal.");
+                }
+            } catch (Exception e) {
+                System.err.println("Es ist ein Fehler aufgetreten: " + e.getMessage());
             }
         }
     }
@@ -84,14 +88,14 @@ public class JavaBankAG {
         return scanner.nextLine();
     }
 
-    private static double askForDouble(String message) {
+    private static double askForDouble(String message) throws NumberFormatException {
         System.out.println(message);
         // Workaround aufgrund fehlendes Newline bei .nextDouble(), siehe https://stackoverflow.com/a/13102066
         String doubleAsString = scanner.nextLine();
         return Double.parseDouble(doubleAsString);
     }
 
-    private static BankAccount getBankAccountForIban(String message) {
+    private static BankAccount getBankAccountForIban(String message) throws NullPointerException {
         String iban = askForString(message);
         return bankAccounts.get(iban);
     }
